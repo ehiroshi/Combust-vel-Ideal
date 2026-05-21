9bece8b3a91e5cd6fc68f26dbba4a00121bf6e31
EDUARDO HIROSHI DE SOUSA MATSUOKA <eduardo.matsuoka@etec.sp.gov.br>
2026-05-21 11:12:28 -0300

    Titulo, imagem do posto, comecei o TextInput, comecei o button

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TextInput } from 'react-native';
 
 export default function App() {
+  let preco_gasolina = 5.59;
+  let preco_etanol = 3.49;
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.texto}>Gasolina ou Alcool? </Text>
+      <Image
+        style={styles.imagem}
+        source={require("./assets/posto.jpg")}
+      />
+      <TextInput
+      style={styles.preco}
+      placeholder='Digite o preço do Etanol: '
+      />
+      <TextInput
+      style={styles.preco}
+      placeholder='Digite o preço da Gasolina: '
+      />
+        <Button
+        title='VERIFICAR VANTAGEM'
+        
+      />
+      
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'green',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+    fontSize: 25,
+    top: -120,
+    backgroundColor: 'yellow',
+    padding: 30,
+    fontWeight: 'bold',
+  },
+  imagem: {
+    top: -120,
+    height: 150,
+    width: 300,
+  },
+  preco: {
+    fontSize: 20,
+    top: -100,
+    fontWeight: 'bold',
+    backgroundColor: 'yellow',
+    margin: 5,
+  },
+
+
 });
